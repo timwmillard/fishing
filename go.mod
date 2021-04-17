665b097770a20e9400b89fc09a32d78786aa8e89
Tim Millard <tim@quickapay.com>
2021-04-18 00:24:53 +1000

    dockertest competitors repo

--- go.mod
+++ go.mod
@@ -4,18 +4,16 @@ go 1.16
 
 require (
 	cloud.google.com/go/firestore v1.5.0
-	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/Microsoft/go-winio v0.4.16 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/containerd/continuity v0.0.0-20210315143101-93e15499afd5 // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
 	github.com/gotestyourself/gotestyourself v1.3.0 // indirect
 	github.com/lib/pq v1.10.0
-	github.com/opencontainers/image-spec v1.0.1 // indirect
 	github.com/opencontainers/runc v1.0.0-rc93 // indirect
 	github.com/ory/dockertest v3.3.5+incompatible
 	github.com/sirupsen/logrus v1.8.1 // indirect
---
+++ postgres/testing/competitors_test.go
@@ -0,0 +1,131 @@
+package testing
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/golang-migrate/migrate/v4"
+	migrateps "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
+	"github.com/stretchr/testify/assert"
+	"github.com/timwmillard/fishing"
+	"github.com/timwmillard/fishing/postgres"
+)
+
+var (
+	db       *sql.DB
+	database = "fishingcomp"
+
+	competitorsRepo *postgres.CompetitorsRepo
+)
+
+var comp1 = fishing.Competitor{
+	ID:           uuid.Must(uuid.NewRandom()),
+	CompetitorNo: "12",
+	Firstname:    "Tim",
+	Lastname:     "Millard",
+	Email:        "tim@example.com",
+	Address1:     "123 Main St",
+	Address2:     "",
+	Suburb:       "Some Town",
+	State:        "VIC",
+	Postcode:     "3000",
+	Phone:        "123456",
+	Mobile:       "04123456",
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if testing.Short() {
+		m.Run()
+		os.Exit(0)
+	}
+	var err error
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "12-alpine",
+		Env: []string{
+			"POSTGRES_USER=root",
+			"POSTGRES_PASSWORD=fish",
+			"POSTGRES_DB=" + database,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: "5999"},
+			},
+		},
+	}
+
+	resource, err := pool.RunWithOptions(&opts)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err = pool.Retry(func() error {
+		var err error
+		db, err = sql.Open("postgres", fmt.Sprintf("postgres://root:fish@localhost:%s/%s?sslmode=disable", resource.GetPort("5432/tcp"), database))
+		if err != nil {
+			return err
+		}
+
+		return db.Ping()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	driver, err := migrateps.WithInstance(db, &migrateps.Config{})
+	if err != nil {
+		log.Fatalf("Could not migrate driver: %s", err)
+	}
+	mig, err := migrate.NewWithDatabaseInstance(
+		"file://../migrations",
+		"postgres", driver)
+	if err != nil {
+		log.Fatalf("Could not migrate new database instance: %s", err)
+	}
+	mig.Up()
+
+	competitorsRepo = postgres.NewCompetitorsRepo(db)
+
+	code := m.Run()
+
+	// When you're done, kill and remove the container
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+	os.Exit(code)
+}
+
+func TestCreate(t *testing.T) {
+	// db.Query()
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	ctx := context.Background()
+
+	c1, err := competitorsRepo.Create(ctx, comp1)
+	assert.NoError(t, err)
+	assert.Equal(t, c1.Firstname, comp1.Firstname)
+	assert.Equal(t, c1.Lastname, comp1.Lastname)
+	assert.Equal(t, c1.Email, comp1.Email)
+
+	c2, err := competitorsRepo.Get(ctx, c1.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, c2.Firstname, comp1.Firstname)
+	assert.Equal(t, c2.Lastname, comp1.Lastname)
+}
