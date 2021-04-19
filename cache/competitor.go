737f9cae7ca1ae3220d52da33ab8f77ca6316d49
Tim Millard <tim@quickapay.com>
2021-04-19 13:02:08 +1000

    rename cache filenames to singular

---
+++ cache/competitor.go
@@ -0,0 +1,44 @@
+package cache
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/timwmillard/fishing"
+)
+
+type Competitors struct {
+	Cache fishing.CompetitorsRepo
+	Repo  fishing.CompetitorsRepo
+}
+
+func (cc *Competitors) List(ctx context.Context) ([]fishing.Competitor, error) {
+	return cc.Repo.List(ctx)
+}
+
+func (cc *Competitors) Get(ctx context.Context, id uuid.UUID) (fishing.Competitor, error) {
+	comp, err := cc.Cache.Get(ctx, id)
+	if err == nil {
+		return comp, nil
+	}
+
+	comp, err = cc.Repo.Get(ctx, id)
+	if err != nil {
+		return fishing.Competitor{}, err
+	}
+	cc.Cache.Create(ctx, comp)
+
+	return comp, nil
+}
+
+func (cc *Competitors) Create(ctx context.Context, c fishing.Competitor) (fishing.Competitor, error) {
+	return cc.Repo.Create(ctx, c)
+}
+
+func (cc *Competitors) Update(ctx context.Context, c fishing.Competitor) (fishing.Competitor, error) {
+	return cc.Repo.Update(ctx, c)
+}
+
+func (cc *Competitors) Delete(ctx context.Context, id uuid.UUID) error {
+	return cc.Repo.Delete(ctx, id)
+}
