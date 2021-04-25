7770cc6bc059af37616095d7ea8c00c817d27f74
Tim Millard <tim@quickapay.com>
2021-04-25 23:20:57 +1000

    test http competitor handler errors

--- http/competitor_test.go
+++ http/competitor_test.go
@@ -3,6 +3,7 @@ package http
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -12,17 +13,6 @@ import (
 	"github.com/timwmillard/fishing"
 	"github.com/timwmillard/fishing/fake"
 	"github.com/timwmillard/fishing/mock"
-)
-
-var (
-	comp1 = fishing.Competitor{
-		Firstname: "Tim",
-		Lastname:  "Millard",
-	}
-	comp2 = fishing.Competitor{
-		Firstname: "John",
-		Lastname:  "Smith",
-	}
 )
 
 func TestCompetitorsHandler_List(t *testing.T) {
@@ -54,4 +44,51 @@ func TestCompetitorsHandler_List(t *testing.T) {
 		t.Logf("want %v\n", want)
 		t.Errorf("deep equal failed")
 	}
+}
+
+func TestCompetitorsHandler_List_empty(t *testing.T) {
+	is := is.New(t)
+
+	mockRepo := &mock.CompetitorRepo{}
+	mockRepo.ListFunc = func(ctx context.Context) ([]fishing.Competitor, error) {
+		return []fishing.Competitor{}, nil
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/competitors/", nil)
+	w := httptest.NewRecorder()
+
+	compHandler := NewCompetitorHandler(mockRepo)
+
+	// SUT
+	compHandler.List(w, req)
+
+	is.Equal(http.StatusOK, w.Code)
+	is.True(mockRepo.ListInvoked)
+
+	got := make([]fishing.Competitor, 0)
+	json.Unmarshal(w.Body.Bytes(), &got)
+
+	is.Equal(len(got), 0)
+}
+
+func TestCompetitorsHandler_List_error(t *testing.T) {
+	is := is.New(t)
+
+	// want := fake.Competitors(2)
+
+	mockRepo := &mock.CompetitorRepo{}
+	mockRepo.ListFunc = func(ctx context.Context) ([]fishing.Competitor, error) {
+		return nil, fmt.Errorf("test")
+	}
+
+	req := httptest.NewRequest(http.MethodGet, "/competitors/", nil)
+	w := httptest.NewRecorder()
+
+	compHandler := NewCompetitorHandler(mockRepo)
+
+	// SUT
+	compHandler.List(w, req)
+
+	is.Equal(http.StatusNotFound, w.Code)
+	is.True(mockRepo.ListInvoked)
 }
