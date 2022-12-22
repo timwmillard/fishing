0c055c06db220a9042e7fcb3a6d2e0948241f2ab
Tim Millard <tim@rapidpaylegal.com>
2022-12-22 15:39:32 +1100

    Hash id

--- hashid.go
+++ hashid.go
@@ -1,3 +1,40 @@
 package fishing
 
+import (
+	"github.com/speps/go-hashids/v2"
+)
+
+var HashSalt = "this is my secret"
+
 type HashID string
+
+func NewHashID(id int) HashID {
+	data := hashids.NewData()
+	data.Salt = HashSalt
+	data.MinLength = 10
+	hash, err := hashids.NewWithData(data)
+	if err != nil {
+		panic(err)
+	}
+
+	str, err := hash.Encode([]int{id})
+	if err != nil {
+		panic(err)
+	}
+	return HashID(str)
+}
+
+func (h HashID) ID() int {
+	data := hashids.NewData()
+	data.Salt = HashSalt
+	data.MinLength = 10
+	hash, err := hashids.NewWithData(data)
+	if err != nil {
+		panic(err)
+	}
+	str := hash.Decode(string(h))
+	if len(str) == 0 {
+		return 0
+	}
+	return str[0]
+}
